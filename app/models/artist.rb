61a8f666d4781c03382078587bc65f9ece6708ab
James Sapp <jameseisapp@gmail.com>
2020-12-26 15:34:42 -0600

    commit tree 336f6e69fb98a4364cfa5a402273b2126f1873aa

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,7 @@
 class Artist < ActiveRecord::Base
+    has_many :songs 
+    has_many :genres, through: :songs
 end
+
+# The name of the other model is pluralized when declaring a has_many association.
+
